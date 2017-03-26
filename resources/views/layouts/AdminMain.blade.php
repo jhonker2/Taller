1ba1705781a73ed140343f68c37c8466a6d9a9f3
jhonyGuaman <jhonker2@hotmail.com>
2017-03-26 14:32:27 -0500

    Cambios de Prueba

--- resources/views/layouts/AdminMain.blade.php
+++ resources/views/layouts/AdminMain.blade.php
@@ -56,7 +56,7 @@
         <![endif]-->
 
 </head>
-<body class="nav-md">
+<body class="nav-sm">
   <div class="container body">
     <div class="main_container">
       <div class="col-md-3 left_col">
